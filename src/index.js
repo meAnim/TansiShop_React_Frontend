05d4b0bc5f421609040f497f4cb654664324af41
FalconZ <animj.emma@gmail.com>
2020-10-16 21:33:03 +0100

    linked index.js to the root element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-const element = < h1 > Hello World < /h1>;
-ReactDom.render(element, document.getElementById('root'));
+ReactDOM.render( < App / > , document.getElementById('root'));
